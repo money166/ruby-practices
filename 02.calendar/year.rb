69c236856cfa4d301faf30fbff998e9dd1db191a
money166 <s1180074@gmail.com>
2021-06-06 13:00:19 +0900

    年のバリデーションが間違っていたため修正。エラー文言もcalコマンドに合わせて修正

--- 02.calendar/year.rb
+++ 02.calendar/year.rb
@@ -5,10 +5,10 @@ class Year
   attr_reader :value
 
   def initialize(year)
-    if year < 1970 && year > 2100
+    if year < 1970 || year > 2100
       # calコマンドとしての制限ではないが最小の仕様
       # 少なくとも1970年から2100年までは正しく表示されるに合わせている
-      raise "cal: #{year} is neither a year number (1970..2100) nor a name"
+      raise "cal: year #{year} not in range 1970..2100"
     end
     @value = year
   end
